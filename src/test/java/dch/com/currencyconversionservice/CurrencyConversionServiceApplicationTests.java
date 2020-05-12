98d71003446be2c0360601a719b1baa24f9c1ce6
Dariusz Chmieła <dariusz.chmiela@ipfdigital.com>
2020-05-12 15:33:58 +0200

    Initial commit

---
+++ src/test/java/dch/com/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dch.com.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
