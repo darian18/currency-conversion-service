88bfc93760f4ffe444fe79f7303416b0a8696fd8
Dariusz Chmieła <dariusz.chmiela@ipfdigital.com>
2020-05-13 12:31:08 +0200

    MSA: Replace Rest by Feign client

--- src/main/java/dch/com/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/dch/com/currencyconversionservice/CurrencyConversionController.java
@@ -1,25 +1,23 @@
 package dch.com.currencyconversionservice;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
 
+    private final CurrencyExchangeServiceProxy proxy;
+
+    public CurrencyConversionController(CurrencyExchangeServiceProxy proxy) {
+        this.proxy = proxy;
+    }
+
     @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
-    public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity){
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("from", from);
-        uriVariables.put("to", to);
-        ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class, uriVariables);
-        CurrencyConversionBean response = responseEntity.getBody();
+    public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+        CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
         return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
     }
 }
