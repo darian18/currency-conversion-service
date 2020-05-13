470ec292c2f6254ef00cafcc670cfd5cf9843de2
Dariusz Chmieła <dariusz.chmiela@ipfdigital.com>
2020-05-13 14:33:02 +0200

    MSA: Include Eureka Naming client

--- src/main/java/dch/com/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/dch/com/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,14 +2,14 @@ package dch.com.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("dch.com.currencyconversionservice")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CurrencyConversionServiceApplication.class, args);
     }
-
 }
